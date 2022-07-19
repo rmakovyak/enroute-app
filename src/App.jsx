1c4ac75870aa8a04127483c273937e689f344f05
Roman Makovyak <roman.makovyak@scoutbee.com>
2022-07-19 15:56:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Enroute app for {import.meta.env.VITE_ORG_NAME}</h1>
+    </div>
+  );
+}
+
+export default App;
