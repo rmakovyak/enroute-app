50737ad36815fc0613206680eda01cd4dc1d6fb8
Roman Makovyak <roman.makovyak@scoutbee.com>
2022-07-19 20:30:44 +0200

    Preview deployments (#1)
    * Preview env
    
    * Fix preview env
    
    * Push another change
    
    * Third update
    
    * And let's push another change
    
    * Try single comment
    
    * Update preview
    
    * Add staging deploy
    
    * Fix
    
    * Fix
    
    * Fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
         </a>
       </div>
       <h1>Enroute app for {import.meta.env.VITE_ORG_NAME}</h1>
+      <p>I am big fat preview change</p>
+      <p>Second smaller change</p>
+      <p>Third smaller change</p>
     </div>
   );
 }
