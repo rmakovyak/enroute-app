6ff23841e0dd3b1997022089e9e2df56abe5b87d
Roman Makovyak <roman.makovyak@scoutbee.com>
2022-07-25 16:08:43 +0200

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img
+            src={`${import.meta.env.BASE_URL}/vite.svg`}
+            className="logo"
+            alt="Vite logo"
+          />
         </a>
       </div>
       <h1>Enroute app for {import.meta.env.VITE_ORG_NAME}</h1>
